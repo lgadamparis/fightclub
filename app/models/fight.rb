62b53942040c9d3ecde58edbaa5bb6c6c3f45f02
jeandumas <jeandumas277@gmail.com>
2021-05-17 17:35:54 +0200

    controllers corrected

--- app/models/fight.rb
+++ app/models/fight.rb
@@ -1,8 +1,8 @@
 class Fight < ApplicationRecord
   belongs_to :location
+  belongs_to :user
   validates :booked, presence: true, uniqueness: true
   validates :result, presence: true, uniqueness: true
   validates :date, presence: true, uniqueness: true
   validates :time, presence: true, uniqueness: true
-  validates :movie, uniqueness: { scope: :list}
 end
