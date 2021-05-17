5739db3cba5e5bf94b2cf83294e4acd5debdcf10
jeandumas <jeandumas277@gmail.com>
2021-05-17 17:32:06 +0200

    models première partie

--- app/models/fight.rb
+++ app/models/fight.rb
@@ -1,2 +1,8 @@
 class Fight < ApplicationRecord
+  belongs_to :location
+  validates :booked, presence: true, uniqueness: true
+  validates :result, presence: true, uniqueness: true
+  validates :date, presence: true, uniqueness: true
+  validates :time, presence: true, uniqueness: true
+  validates :movie, uniqueness: { scope: :list}
 end
