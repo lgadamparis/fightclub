56a6eb4f0f8de88f2d6a823f525b66b88c9259ec
Cheran Nathan <cheran.nathan@gmail.com>
2021-05-18 17:38:40 +0200

    index page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
-  enum skills: ["boxe", "muay thai", "mma", "la bagarre", "ju-jitsu", "karaté", "fart", "no skills, I'm a damn shit"]
+  #enum skills: ["boxe", "muay thai", "mma", "la bagarre", "ju-jitsu", "karaté", "fart", "no skills, I'm a damn shit"]
   has_many :fights
   has_many :locations
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :pseudo, presence: true, uniqueness: true
-  validates :skills, presence: true
+  #validates :first_name, presence: true
+  #validates :last_name, presence: true
+  #validates :pseudo, presence: true, uniqueness: true
+  #validates :skills, presence: true
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
