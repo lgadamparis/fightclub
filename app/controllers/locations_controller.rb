bd6146aaaa1a3e0248d5ee694cb67f2b39ee84d1
Sebastien Bruno <sebastienbruno122@gmail.com>
2021-05-18 10:40:20 +0200

    begining of controller

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,2 +1,35 @@
 class LocationsController < ApplicationController
+
+  def index
+    @locations = Location.all
+  end
+
+  def show
+    @figlocation = Fight.new
+  end
+
+  def new
+    @location = Location.new
+  end
+
+  def create
+    @location = Location.new(location_params)
+    if @location.save
+      redirect_to XXX
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @location.destroy
+    redirect_to XXX
+  end
+
+private
+
+def list_params
+  params.require(:location).permit(:name, :location)
+end
+
 end
