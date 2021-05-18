d8be2ddb5f5a9c6c275a9648d2afddee374de2b3
jeandumas <jeandumas277@gmail.com>
2021-05-18 11:17:16 +0200

    models done mothafucka

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,6 @@
 class Location < ApplicationRecord
-  has_many :user, through: :fight
+  belongs_to :user
+  has_many :fights
   validates :location, presence: true, uniqueness: true
   validates :name, presence: true, uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
 
-
+  SKILL_LIST = ["boxe", "muay thai", "mma", "la bagarre", "ju-jitsu", "karaté", "fart", "no skills, I'm a damn shit"]
+  has_many :fights
+  has_many :locations
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :pseudo, presence: true, uniqueness: true
+  validates :skills, presence: true, inclusion: { in: SKILL_LIST }
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
