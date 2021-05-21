78da5bd705625510ade647dbf7e48e245479d52d
jeandumas <jeandumas277@gmail.com>
2021-05-21 12:28:47 +0200

    çaavance

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :locations, only: [:index, :new, :create, :show, :edit, :destroy] do
-    resources :fights, only: [:new, :create]
+    resources :fights, only: [:new, :create, :destroy]
   end
   resources :fights , only: :destroy
 
